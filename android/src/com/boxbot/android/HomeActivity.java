672f6241a10c7f80aa32fdd713a47a9e831926ca
Arnob Mallick <amallick@andrew.cmu.edu>
2014-09-13 05:26:59 -0400

    minor edit to home page

--- android/src/com/boxbot/android/HomeActivity.java
+++ android/src/com/boxbot/android/HomeActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 
 public class HomeActivity extends ActionBarActivity {
 
+	private View bot_view;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
